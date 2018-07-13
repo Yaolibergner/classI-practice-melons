8a8c65041bf1a417d4321d1d40e71018c9bb212b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-12 17:40:05 -0700

    Completed First part of Melon Farm.

--- harvest.py
+++ harvest.py
@@ -34,25 +34,65 @@ class MelonType(object):
         # Fill in the rest
         self.code = new_code
 
+    def __repr__(self):
+
+    	return f"{self.name}"
+
 
 def make_melon_types():
     """Returns a list of current melon types."""
 
     all_melon_types = []
 
+    Musk = MelonType("musk", "1998", "green", True, True, "Muskmelon")
+    Musk.add_pairing("mint")
+    Cas = MelonType("cas", "2003", "orange", False, False,"Casaba")
+    Cas.add_pairing("mint")
+    Cas.add_pairing("strawberries")
+    Cren = MelonType("cren", "1996", "green", False, False, "Crenshaw")
+    Cren.add_pairing("proscuitto")
+    YWaterm = MelonType("yw", "2013", "yellow", True, True, "YWatermelon")
+    YWaterm.add_pairing("ice cream")
+
+    all_melon_types.extend([Musk, Cas, Cren, YWaterm])
+
+
     # Fill in the rest
 
     return all_melon_types
+
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
     # Fill in the rest
+   # melon_types = make_melon_types()
+    for melon in melon_types:
+    	print(f"{melon.name} pairs with")
+    	for pair in melon.pairings:
+    		print("- {}".format(pair))
+
+
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
     # Fill in the rest
+    melontype_by_code = {}
+
+    for melon in melon_types:
+
+    	melontype_by_code[melon.code] = melon
+
+    return melontype_by_code
+
+
+melon_types = make_melon_types()
+print_pairing_info(melon_types)
+print(make_melon_type_lookup(melon_types))
+
+
 
 ############
 # Part 2   #
